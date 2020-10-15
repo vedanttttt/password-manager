63157d1ece39dd3a82234b5c66b2cc2491172349
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2020-10-15 20:46:34 +0530

    editing and updating records,displaying alert messages

--- myapps/modules/employee.js
+++ myapps/modules/employee.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/employees', {useNewUrlParser: true});
 var conn =mongoose.Collection;
 
 var employeeSchema =new mongoose.Schema({
--- myapps/routes/index.js
+++ myapps/routes/index.js
@@ -8,7 +8,7 @@ var employee = employeeModel.find({});
 router.get('/', function(req, res, next) {
   employee.exec(function(err,data){
     if(err) throw err;
-    res.render('index', { title: 'Employee Records', records: data });
+    res.render('index', { title: 'Employee Records', records: data,success: '' });
   });
 });
 
@@ -20,7 +20,7 @@ router.post("/",function(req,res,next){
     etype : req.body.emptype,
     hourlyrate : req.body.hrlyrate,
     totalHour : req.body.ttlhr,
-    total: parseInt(req.body.hrlyrate) * parseInt(req.body.ttlhr)
+   // total: parseInt(req.body.hrlyrate) * parseInt(req.body.ttlhr)
   });
 
   console.log(empDetails);
@@ -29,10 +29,92 @@ router.post("/",function(req,res,next){
     if(err) throw err;
     employee.exec(function(err,data){
       if(err) throw err;
-      res.render('index',{title: 'Employee Records', records: data});
+      res.render('index',{title: 'Employee Records', records: data,success: 'Record Inserted Successfully'});
     });
   });
 
+});
+
+router.post('/search',function(req,res,next){
+
+  var flrtName = req.body.flrtname;
+  var fltrEmail = req.body.fltremail;
+  var fltrType = req.body.fltremptype;
+
+  if(flrtName!='' && fltrEmail!='' && fltrType!=''){
+
+    var filterParameter={ $and:[{name : flrtName},
+    {$and:[{email:fltrEmail},{etype:fltrType}]}
+    ] 
+    }
+  }
+  else if(flrtName!='' && fltrEmail=='' && fltrType!=''){
+
+    var filterParameter={$and:[{name:flrtName},{etype:fltrType}]}
+  }
+  else if(flrtName=='' && fltrEmail!='' && fltrType!=''){
+
+    var filterParameter={$and:[{email:fltrEmail},{etype:fltrType}]}
+  }
+  else if(flrtName=='' && fltrEmail=='' && fltrType!=''){
+
+    var filterParameter={etype:fltrType}
+  }
+  else{
+    var filterParameter={}
+  }
+
+  var employeeFilter = employeeModel.find(filterParameter);
+
+  employeeFilter.exec(function(err,data){
+    if(err) throw err;
+    res.render('index',{title: 'Employee records',records :data});
+  });
+});
+
+router.get('/delete/:id',function(req,res,next){
+
+  var id = req.params.id;
+  var del = employeeModel.findByIdAndDelete(id);
+
+  del.exec(function(err){
+    if(err)throw err;
+    employee.exec(function(err,data){
+      if(err) throw err;
+      res.render('index',{title: 'Employee Records', records: data,success: 'Record Deleted Successfully'});
+    });
+  });
+});
+
+router.get('/edit/:id',function(req,res,next){
+
+  var id = req.params.id;
+  var edit = employeeModel.findById(id);
+
+  edit.exec(function(err,data){
+    if(err) throw err;
+    res.render('edit',{title: 'Edit Employee Records',records:data});
+  });
+});
+
+router.post('/update',function(req,res,next){
+
+  var update = employeeModel.findByIdAndUpdate(req.body.id,{
+    name : req.body.uname,
+    email : req.body.email,
+    etype : req.body.emptype,
+    hourlyrate : req.body.hrlyrate,
+    totalHour : req.body.ttlhr,
+    //total: parseInt(req.body.hrlyrate) * parseInt(req.body.ttlhr)
+  });
+
+  update.exec(function(err,data){
+    if(err) throw err;
+    employee.exec(function(err,data){
+      if(err) throw err;
+      res.render('index',{title: 'Employee Records', records: data,success: 'Record Updated Successfully'});
+    });
+  });
 });
 
 module.exports = router;
